383f75825c64f3c65a0ace02d6e1d52435e11ee4
wanhsuan wu <g010329@gmail.com>
2022-04-29 20:37:45 +0800

    1-9.catching errors with ts

--- fetch-json/README.md
+++ fetch-json/README.md
@@ -3,3 +3,5 @@
 `tsc index.ts`
 `node index.js`
 `tsc index.ts` 和 `node index.js`可合併成 `ts-node index.ts`
+
+API: https://jsonplaceholder.typicode.com/todos/1
--- fetch-json/index.ts
+++ fetch-json/index.ts
@@ -2,4 +2,20 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
-axios.get(url).then((res) => console.log(res.data));
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((res) => {
+  const todo = res.data as Todo;
+
+  const { id, title, completed } = todo;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+});
