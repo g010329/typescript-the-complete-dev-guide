ee9f7274bb9c29ba272e92347335bb51d28ce84c
wanhsuan wu <g010329@gmail.com>
2022-04-30 17:15:47 +0800

    3-16~19.type annotation

---
+++ fearures/annotations/variables.ts
@@ -0,0 +1,31 @@
+// Type Annotation
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = false;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in object
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = { x: 10, y: 20 };
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// types:
+// 1. primitive types: number, boolean, void, undefined, string, symbol, null
+// 2. object types: functions, array, classes, objects
