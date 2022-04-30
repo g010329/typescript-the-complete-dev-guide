6f0d05c15e7a4730dbfc21919b89b88aafd182b5
wanhsuan wu <g010329@gmail.com>
2022-04-30 17:48:38 +0800

    3-23.delayd initialization

--- fearures/annotations/variables.ts
+++ fearures/annotations/variables.ts
@@ -43,3 +43,14 @@ const json = '{ "x": 10, "y": 20 }';
 // const coordinates = JSON.parse(json); // mouseover coordinates see 'any'
 const coordinates: { x: number; y: number } = JSON.parse(json); // mouseover coordinates
 console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ["red", "green", "blue"];
+// let foundWord;
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
