a36e3ce96b98da38162d6afb1bec6600ad9e275d
wanhsuan wu <g010329@gmail.com>
2022-05-01 13:20:14 +0800

    3-28.void and never

--- fearures/annotations/functions.ts
+++ fearures/annotations/functions.ts
@@ -22,3 +22,22 @@ function devided(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+// const throwError2 = (message: string): string => {
+//   if (!message) {
+//     throw new Error(message);
+//   }
+//   return message;
+// };
+// const throwError3 = (message: string): void => {
+//   if (!message) {
+//     throw new Error(message);
+//   }
+// };
