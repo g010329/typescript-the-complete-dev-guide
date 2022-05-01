d74f1669804e7d0fc29f44034a9d0aa245def155
wanhsuan wu <g010329@gmail.com>
2022-05-01 21:52:23 +0800

    8-47.instance method modifiers

--- fearures/annotations/classes.ts
+++ fearures/annotations/classes.ts
@@ -1,20 +1,30 @@
 // Classes -> blueprint to create an object with some fields(values) and methods(functions) to represent a thing
 
+// Modifier
+// public: this method can be called anywhere, anytime
+// private: this method can only be called by other methods in this class
+// protected: this method can be called by other methods in this class, or by other method in child classes(we can't change modifier in child class)
+
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
-  honk(): void {
+  protected honk(): void {
     console.log("beep");
   }
 }
 
+// const vehicle = new Vehicle();
+// vehicle.honk();
+
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car();
-car.drive();
+car.startDrivingProcess();
 car.honk();
