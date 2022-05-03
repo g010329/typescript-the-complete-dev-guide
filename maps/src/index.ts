c576c4c0fbc7e0225978d2db0b98858fe22e1833
wanhsuan wu <g010329@gmail.com>
2022-05-03 21:36:25 +0800

    9-68.duplicate code

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -3,11 +3,12 @@ import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap("map");
-// const company = new Company();
 
 // console.log(user);
 // console.log(company);
 
 // const customMap = new CustomMap("map");
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
