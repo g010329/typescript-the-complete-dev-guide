46ac2c391fb44b7f1fb4e997711806fbda24aba2
wanhsuan wu <g010329@gmail.com>
2022-05-03 21:34:10 +0800

    9-67.adding markers

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -2,10 +2,12 @@ import { User } from "./User";
 import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 
-// const user = new User();
+const user = new User();
+const customMap = new CustomMap("map");
 // const company = new Company();
 
 // console.log(user);
 // console.log(company);
 
-const customMap = new CustomMap("map");
+// const customMap = new CustomMap("map");
+customMap.addUserMarker(user);
