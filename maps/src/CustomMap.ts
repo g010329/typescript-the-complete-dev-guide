c0f028efea95cf3df91790f17d16c69f5e36d92d
wanhsuan wu <g010329@gmail.com>
2022-05-04 19:50:07 +0800

    9-72.showing popup windows

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,10 +1,9 @@
-import { User } from "./User";
-import { Company } from "./Company";
-
 // Instructions to every other class
 // on how they can be arguement to `addMarker`
 interface Mappable {
   location: { lat: number; lng: number };
+
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -18,12 +17,21 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
+    // addMarker(mappable: User | Company): void {
     // * The result of 'or' operator here is that TS is going to limit the number of properties that we can refer to on this mappable thing
     // * TS 會自動比對 User 和 Company 都有的 property，所以此處只剩下 location
 
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: { lat: mappable.location.lat, lng: mappable.location.lng },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
