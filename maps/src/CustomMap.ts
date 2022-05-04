75bac010d2b118a626becfe3ec9761cb3e02e149
wanhsuan wu <g010329@gmail.com>
2022-05-04 19:22:47 +0800

    9-70.restricting access with interfaces

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,6 +1,12 @@
 import { User } from "./User";
 import { Company } from "./Company";
 
+// Instructions to every other class
+// on how they can be arguement to `addMarker`
+interface Mappable {
+  location: { lat: number; lng: number };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -11,21 +17,7 @@ export class CustomMap {
     });
   }
 
-  // addUserMarker(user: User): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: { lat: user.location.lat, lng: user.location.lng },
-  //   });
-  // }
-
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: { lat: company.location.lat, lng: company.location.lng },
-  //   });
-  // }
-
-  addMarker(mappable: User | Company): void {
+  addMarker(mappable: Mappable): void {
     // * The result of 'or' operator here is that TS is going to limit the number of properties that we can refer to on this mappable thing
     // * TS 會自動比對 User 和 Company 都有的 property，所以此處只剩下 location
 
