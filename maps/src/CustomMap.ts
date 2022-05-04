3c716a312552022256e8cc0fbe575e3a72d4a99b
wanhsuan wu <g010329@gmail.com>
2022-05-04 19:54:37 +0800

    9-74.optional implements clauses

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,6 +1,6 @@
 // Instructions to every other class
 // on how they can be arguement to `addMarker`
-interface Mappable {
+export interface Mappable {
   location: { lat: number; lng: number };
 
   markerContent(): string;
