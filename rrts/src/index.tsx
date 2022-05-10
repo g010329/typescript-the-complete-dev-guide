8a272864143bbe33e858d8b25ba40cd72ca01c0f
wanhsuan wu <g010329@gmail.com>
2022-05-10 21:28:43 +0800

    17-266.confusing component state

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -5,9 +5,34 @@ interface AppProps {
   color?: string;
 }
 
+// interface AppState {
+//   counter: number;
+// }
+
 class App extends React.Component<AppProps> {
+  //  AppState
+  state = { counter: 0 };
+  // constructor(props: AppProps) { // 二擇一
+  //   super(props);
+
+  //   this.state = { counter: 0 };
+  // }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter++ });
+  };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter-- });
+  };
+
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
