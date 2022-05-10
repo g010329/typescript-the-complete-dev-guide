36ed48a0f550e2e106e655c3cbef0cab8f0e5012
wanhsuan wu <g010329@gmail.com>
2022-05-10 22:24:35 +0800

    17-273.validating store structure

--- rrts/src/reducers/index.ts
+++ rrts/src/reducers/index.ts
@@ -1,3 +1,11 @@
 import { combineReducers } from "redux";
+import { todosReducer } from "./todos";
+import { Todo } from "../actions";
 
-export const reducers = combineReducers({ counter: () => 1 });
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({ todos: todosReducer });
+
+// 此處檢查 todos的值todoReducer回傳值型別是 Todos[]
