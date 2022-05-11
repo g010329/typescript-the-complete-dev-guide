30cf68c195c9c2cafac9a0e12825de7be0f7ff39
wanhsuan wu <g010329@gmail.com>
2022-05-11 20:23:58 +0800

    17-278.expressing actoins as type union

--- rrts/src/actions/types.ts
+++ rrts/src/actions/types.ts
@@ -1,4 +1,8 @@
+import { FetchedTodosAction, DeleteTodoAction } from "./todos";
+
 export enum ActionTypes {
   fetchTodos,
   deleteTodo,
 }
+
+export type Action = FetchedTodosAction | DeleteTodoAction;
--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -1,12 +1,9 @@
-import { Todo, FetchedTodosAction, ActionTypes } from "../actions";
+import { Todo, Action, ActionTypes } from "../actions";
 
 // state:Todo[] => state 的型別是含 Todo 的 Array
 // =[] => 初始值是空 Array
 // TODO: 此處限制 action: FetchedTodosAction 不符合實作情境
-export const todosReducer = (
-  state: Todo[] = [],
-  action: FetchedTodosAction
-) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
