22a717e5545b3304bea176859ad49a6047cd647a
wanhsuan wu <g010329@gmail.com>
2022-05-22 20:39:52 +0800

    10-80.sorter scaffolding

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hi there!");
+// class Sorter {
+//   collection: number[];
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+
+//   sort(): void {}
+// }
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
